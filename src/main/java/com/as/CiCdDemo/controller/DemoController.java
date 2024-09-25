b81d5225f99432498da97421d2ab3e556291b1e6
Abhay singh <abhaysinghjprk6@gmail.com>
2024-09-25 16:06:52 +0530

    first commit for master

---
+++ src/main/java/com/as/CiCdDemo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.as.CiCdDemo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class DemoController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> demoResponse(){
+        return new ResponseEntity<>("hello from master branch !!", HttpStatus.ACCEPTED);
+    }
+}
