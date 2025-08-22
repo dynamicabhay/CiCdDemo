965857ab79be3b9a8a5eba13de07143f44893820
Abhay <abhay@Ruchitas-MacBook-Air.local>
2025-08-22 11:45:55 +0530

    added debuging

--- src/main/java/com/as/CiCdDemo/configs/FirebaseConfig.java
+++ src/main/java/com/as/CiCdDemo/configs/FirebaseConfig.java
@@ -3,6 +3,8 @@ package com.as.CiCdDemo.configs;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +13,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
+@Slf4j
 public class FirebaseConfig {
 
     @Value("${fb.config.path}")
@@ -18,6 +21,7 @@ public class FirebaseConfig {
 
     @PostConstruct
     public void init() throws IOException {
+        log.debug("the path of file is :: " + FbConfigPath);
         if (FirebaseApp.getApps().isEmpty()) {
             FileInputStream serviceAccount = new FileInputStream(FbConfigPath);
             FirebaseOptions options = FirebaseOptions.builder()
