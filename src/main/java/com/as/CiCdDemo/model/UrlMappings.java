f979f4b4822fe75accf25c40deff3e875fd21046
Abhay <abhay@Ruchitas-MacBook-Air.local>
2025-08-26 19:40:37 +0530

    connected postgresql db

---
+++ src/main/java/com/as/CiCdDemo/model/UrlMappings.java
@@ -0,0 +1,38 @@
+package com.as.CiCdDemo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "urls")
+public class UrlMappings {
+
+    @Id
+    private long id;
+
+    @Column(unique = true,nullable = false,length = 7)
+    private String shortKey;
+    @Column(nullable = false)
+    private String originalUrl;
+    @Column(nullable = false)
+    private Instant createdAt;
+    @Column
+    private Instant expiresAt;
+    @Column
+    private long visitCount;
+
+    public UrlMappings(String originalUrl,Instant createdAt, String shortKey,long id){
+        this.originalUrl = originalUrl;
+        this.shortKey = shortKey;
+        this.createdAt = createdAt;
+        this.id = id;
+    }
+
+}
--- src/main/java/com/as/CiCdDemo/service/UrlShortenerService.java
+++ src/main/java/com/as/CiCdDemo/service/UrlShortenerService.java
@@ -4,47 +4,69 @@ import com.as.CiCdDemo.dto.UrlShortnerRequest;
 import com.as.CiCdDemo.dto.UrlShortnerResponse;
 import com.as.CiCdDemo.exceptions.AliasAlreadyTakenException;
 import com.as.CiCdDemo.exceptions.KeyNotFoundException;
+import com.as.CiCdDemo.model.UrlMappings;
+import com.as.CiCdDemo.repository.UrlMappingRepository;
+import jakarta.persistence.EntityManager;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
+@Slf4j
 public class UrlShortenerService {
 
     public Base62 encoder;
-    private Map<String,String> urlStore;
-    private AtomicInteger counter;
-
+    private UrlMappingRepository urlMappingRepository;
     private String baseUrl;
+    EntityManager entityManager;
 
-    public UrlShortenerService(Base62 encoder,@Value("${server.domain}") String domain){
+    public UrlShortenerService(Base62 encoder,@Value("${server.domain}") String domain,UrlMappingRepository urlMappingRepository,EntityManager entityManager){
 
         this.encoder = encoder;
-        urlStore = new HashMap<>();
-        counter = new AtomicInteger(0);
+        this.urlMappingRepository = urlMappingRepository;
         baseUrl = String.format("https://%s/s/", domain);
+        this.entityManager = entityManager;
     }
 
-    public UrlShortnerResponse process(UrlShortnerRequest request) throws AliasAlreadyTakenException {
-        String key = null;
-        if(request.getAlias() != null){
-            key = request.getAlias().trim();
-            if(urlStore.containsKey(key)) throw new AliasAlreadyTakenException(key);
+    @Transactional
+    public UrlShortnerResponse process(UrlShortnerRequest request) {
+        try {
+            Long nextId = ((Number) entityManager
+                    .createNativeQuery("SELECT nextval('url_sequence')")
+                    .getSingleResult()).longValue();
+
+            String originalUrl = request.getUrl().trim();
+            String key = encoder.encode(nextId);
+            UrlMappings newUrl = new UrlMappings(originalUrl,Instant.now(),key,nextId);
+
+            UrlMappings entityWithId = urlMappingRepository.save(newUrl);
+            String shortUrl = (baseUrl + key);
+            return UrlShortnerResponse.builder().shortUrl(shortUrl).build();
+        } catch (Exception e) {
+
+            log.info(e.getMessage());
+            throw new RuntimeException(e);
         }
-        else {
-            long count = counter.incrementAndGet();
-            key = encoder.encode(count);
-        }
-        urlStore.put(key, request.getUrl());
-        String shortUrl = (baseUrl + key);
-        return UrlShortnerResponse.builder().shortUrl(shortUrl).build();
     }
 
+    @Transactional
     public String getUrl(String key) throws KeyNotFoundException {
-        if(urlStore.containsKey(key)) return urlStore.get(key);
-        else throw new KeyNotFoundException(key);
+        if(key == null || key.isBlank()) throw new RuntimeException("URL not found ");
+
+            UrlMappings mapping = urlMappingRepository.findByShortKey(key.trim()).orElseThrow(() -> new RuntimeException());
+            mapping.setVisitCount(mapping.getVisitCount()+1);
+
+            System.out.println("shortKey: " + key);
+
+            return mapping.getOriginalUrl();
+
     }
+
+
 }
